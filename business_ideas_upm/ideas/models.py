b8719ac3c05412590d47a10a772c9e39dcc33942
Gael-Bernard <gael.berbere@gmail.com>
2022-11-18 09:32:05 +0100

    Created base DB models for ideas

--- business_ideas_upm/ideas/models.py
+++ business_ideas_upm/ideas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class BusinessIdea(models.Model):
+    title = models.TextField(max_length=128)
+    body = models.TextField(max_length=4096)
+    username = models.TextField(max_length=32)
+
+
+class IdeaComment(models.Model):
+    idea: models.ForeignKey(BusinessIdea, on_delete=models.CASCADE)
+    title = models.TextField(max_length=128)
+    body = models.TextField(max_length=4096)
+    username = models.TextField(max_length=32)
