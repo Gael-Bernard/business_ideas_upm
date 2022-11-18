0dcdf169c4228dfe5aa499b5e264d26489f3804d
Gael-Bernard <gael.berbere@gmail.com>
2022-11-18 10:10:51 +0100

    Edited models to add publish_date

--- business_ideas_upm/ideas/models.py
+++ business_ideas_upm/ideas/models.py
@@ -6,10 +6,11 @@ class BusinessIdea(models.Model):
     title = models.TextField(max_length=128)
     body = models.TextField(max_length=4096)
     username = models.TextField(max_length=32)
+    publish_date = models.DateTimeField()
 
 
 class IdeaComment(models.Model):
     idea: models.ForeignKey(BusinessIdea, on_delete=models.CASCADE)
     body = models.TextField(max_length=4096)
     username = models.TextField(max_length=32)
-    publish_date = models.DateTimeField(default=Date(2022, 1, 1))
+    publish_date = models.DateTimeField()
