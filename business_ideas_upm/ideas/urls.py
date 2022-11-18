a81321ff5d1d5749aaec3bb9ab91434b316d725d
Gael-Bernard <gael.berbere@gmail.com>
2022-11-18 10:21:45 +0100

    Implemented ideas list (10 latest ideas)

--- business_ideas_upm/ideas/urls.py
+++ business_ideas_upm/ideas/urls.py
@@ -3,6 +3,10 @@ from django.urls import include, path
 from . import views
 
 
+app_name = "ideas"
+
 urlpatterns = [
-    path("", views.list, name="list"),
+    path("", views.list, name="index"),
+    path("list", views.list, name="list"),
+    path("idea/<int:idea_id>", views.idea, name="idea")
 ]
--- business_ideas_upm/ideas/views.py
+++ business_ideas_upm/ideas/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from .models import BusinessIdea
 
 # Create your views here.
+
 def list(request):
-    return HttpResponse("Yo, welcome to our website")
+    ideas_list = BusinessIdea.objects.order_by("-publish_date")[:10]
+    template = loader.get_template('ideas/list.html')
+    context = {
+        'ideas_list': ideas_list,
+    }
+    print(ideas_list)
+    print(ideas_list[0].title)
+    return HttpResponse(template.render(context, request))
+
+
+def idea(request, idea_id):
+    return HttpResponse("Page of business idea "+str(idea_id))
